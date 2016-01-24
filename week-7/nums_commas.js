605d3d5f3b706fc13392cd4c3718c18ae3da31bd
JonSchwartz93 <josch@umich.edu>
2016-01-24 00:25:53 -0500

    update nums_commas.js

--- week-7/nums_commas.js
+++ week-7/nums_commas.js
@@ -1,24 +1,20 @@
 // Separate Numbers with Commas in JavaScript **Pairing Challenge**
 
 
-// I worked on this challenge with: Tim Kelly .
+// I worked on this challenge with: Tim Kelly.
 
-// Pseudocode
-
+// Pseudocode:
 /*
 Input: Integer as an argument
 Output: comma-separated integer as a string
 Steps:
+	Covert to STRING or ARRAY to count the total number of digits in the integer.
 
-Covert to STRING or ARRAY to count the total number of digits in the integer.
-
-Possibly reverse number.  Count every three digits and add a comma afterwards.  
-Un-reverse string and RETURN
-
+	Possibly reverse number.  Count every three digits and add a comma afterwards.  
+	Un-reverse string and RETURN
 */
 
-
-// Initial Solution
+// Initial Solution:
 /*
 function addCommas(ourNumber){
   //convert ourNumber from integer to string to array
@@ -49,7 +45,7 @@ function addCommas(ourNumber){
 }
 */
 
-// Refactored Solution
+// Refactored Solution:
 
 function addCommas(ourNumber){
   var stringNum = ourNumber.toString(), 
@@ -76,9 +72,24 @@ function addCommas(ourNumber){
   return unreversedArray.join("");
 }
 
-// Your Own Tests (OPTIONAL)
+// Your Own Tests (OPTIONAL):
 
 var testNumber = -5467344444444232;
 console.log("The test: " + addCommas(testNumber))
 
-// Reflection
+// Reflection:
+// What was it like to approach the problem from the perspective of JavaScript? Did you approach the problem differently?
+// It was very interesting approaching this problem from the perspective of JavaScript. I'm definitely still getting used to the new syntax, but thinking through the actual problem is actually quite similar. Essentially what my pair and I did was pseudocode on a high-level to create a blueprint/map of our thought-process, and then dive into the code.
+
+// What did you learn about iterating over arrays in JavaScript?
+// Again, I'm definitely still getting used to the differences in syntax between JS and Ruby, but iterating over arrays using loops and counters is, in my opinion, more difficult. This is probably due to the fact that I'm still in the early stages of my understanding of JS, so I'm interested to see if there are easier, more direct ways (like the methods in Ruby) to iterate over an array.
+
+// What was different about solving this problem in JavaScript?
+// The syntax of iteration is slightly different in JavaScript which therefore caused my pair and I to take a varied approach from what we had taken when we solved this same problem in Ruby.
+
+// What built-in methods did you find to incorporate in your refactored solution?
+// - toString()
+// - .split
+// - .length
+// - .unshift
+// - .join
