fc11cb884ca51143b6fefa4fa69767f918c1b4a7
JonSchwartz93 <josch@umich.edu>
2016-01-24 23:47:51 -0500

    add group-project

---
+++ week-7/group-project/group_project_solution.js
@@ -0,0 +1,61 @@
+// Person 1: Tests to User Stories
+
+// SUM:
+// As a user, I want to create a Function called "sum" 
+// that returns the sum of a group of numbers. The 
+// sum should be returned in two scenarious: if the 
+// group has an odd amount of numbers AND an even 
+// amount of numnbers.
+
+//Pseudocode:
+
+
+
+
+//Initial Solution:
+
+
+
+
+// MEAN:
+// As a user, I want to create a Function called "mean" 
+// that returns the mean of a group of numbers. The 
+// mean should be returned in two scenarios: when the 
+// group has an odd amount of numbers AND an even 
+// amount of numnbers.
+
+//Pseudocode:
+
+
+
+
+//Initial Solution:
+
+
+
+
+// MEDIAN:
+// As a user, I want to create a Function called "median" 
+// that returns the median of a group of numbers. The 
+// median should be returned in two scenarios: when the 
+// group has an odd amount of numbers AND an even 
+// amount of numnbers.
+
+//Pseudocode:
+
+
+
+
+//Initial Solution:
+
+
+
+
+
+// Person 1: Summary of Tests (which ones passed/failed):
+
+
+// Reflection:
+
+
+
